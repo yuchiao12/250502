1a06e1acec748b2f88e1ddc49a0a55fd484f14a9
yuchiao12 <yuchiaochiaochiao@gmail.com>
2025-05-02 10:25:04 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,9 @@ function setup() {
 
 function draw() {
   background(139, 69, 19); // 確保背景保持棕色
-  image(capture, (width - capture.width) / 2, (height - capture.height) / 2); // 將影像置中
+  translate(width / 2, height / 2); // 將原點移到畫布中心
+  scale(-1, 1); // 水平翻轉影像
+  image(capture, -capture.width / 2, -capture.height / 2); // 將影像置中
 }
 
 function windowResized() {
